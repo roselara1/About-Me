1a1fa9bafdb16ab7430b1772b72000aed6f3ffe0
Rosie Lara <roselara1@gmail.com>
2018-01-18 19:31:32 -0800

    Checking branch

---
+++ AnaTest.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function fiveQuestions(){
+var questionOne = prompt ('Does Ana have any pets?', 'type yes or no').toLowerCase
+
+
+
+}
--- app.js
+++ app.js
@@ -13,12 +13,12 @@ var user = prompt('What is your name?');
 alert('Hi ' + user + ' I\'m so glad you want to play my game to try to figure me out.');
 
 //Question 1
+function fiveQuestions(){
+var question1 = prompt('Does Ana have any pets? ' , 'type yes or no').toLowerCase();
 
-var answer = prompt('Does Ana have any pets? ' , 'type yes or no').toLowerCase();
+console.log(question1);
 
-console.log(answer);
-
-if (answer === 'yes' || answer === 'y') {
+if (question1 === 'yes' || question1 === 'y') {
     //if its correct give them a point
     userPoints++; //If the answer is yes, they're going to get a point
 } else {
@@ -26,16 +26,16 @@ if (answer === 'yes' || answer === 'y') {
     alert('Sorry, but I do!');
 
 }
-
 alert(' You have ' + userPoints + ' points.')
-
+}
+fiveQuestions();
 //Question 2
+function fiveQuestions(){
+var question2 = prompt('Is Ana an only child? ' , 'type yes or no').toLowerCase();
 
-var onlyChild = prompt('Is Ana an only child? ' , 'type yes or no').toLowerCase();
+console.log(question2);
 
-console.log(onlyChild);
-
-if (onlyChild === 'no' || onlyChild === 'n') {
+if (question2 === 'no' || question2 === 'n') {
     //if its correct give them a point
     userPoints++; //If the answer is yes, they're going to get a point
 } else {
@@ -44,62 +44,64 @@ if (onlyChild === 'no' || onlyChild === 'n') {
 
 }
 alert(' You have ' + userPoints + ' points.')
-
-//Question 3
-
-var anaDrive = prompt('Does Ana know how to drive?' , 'type yes or no').toLowerCase();
-
-console.log(anaDrive);
-
-if (anaDrive === 'yes' || anaDrive === 'y') {
-    //if its correct give them a point
-    userPoints++; //If the answer is yes, they're going to get a point
-} else {
-    //if its not correct, tell them to try again
-    alert('Wrong again! ..but I do hate to drive. Please try again!');
-
 }
-alert(' You have ' + userPoints + ' points.')
+fiveQuestions();
 
-//Question 4
+// //Question 3
 
-var gameOfThrones = prompt(' Does Ana like Game of Thrones?' , 'type yes or no').toLowerCase();
+// var anaDrive = prompt('Does Ana know how to drive?' , 'type yes or no').toLowerCase();
 
-console.log(gameOfThrones);
+// console.log(anaDrive);
 
-if (gameOfThrones === 'no' || gameOfThrones === 'n') {
-    //if its correct give them a point
-    userPoints++; //If the answer is yes, they're going to get a point
-} else {
-    //if its not correct, tell them to try again
-    alert('Sorry, but I have never seen an episode of that show.. Try again! :(');
+// if (anaDrive === 'yes' || anaDrive === 'y') {
+//     //if its correct give them a point
+//     userPoints++; //If the answer is yes, they're going to get a point
+// } else {
+//     //if its not correct, tell them to try again
+//     alert('Wrong again! ..but I do hate to drive. Please try again!');
 
-}
-alert(' You have ' + userPoints + ' points.')
+// }
+// alert(' You have ' + userPoints + ' points.')
 
-//Question 5
+// //Question 4
 
-var otherLanguage = prompt(' Does Ana speak any languages other than English?' , 'type yes or no').toLowerCase();
+// var gameOfThrones = prompt(' Does Ana like Game of Thrones?' , 'type yes or no').toLowerCase();
 
-console.log(otherLanguage);
+// console.log(gameOfThrones);
 
-if (otherLanguage === 'yes' || otherLanguage === 'y') {
-    //if its correct give them a point
-    userPoints++; //If the answer is yes, they're going to get a point
-} else {
-    //if its not correct, tell them to try again
-    alert('Wrong, Ana knows how to speak Spanish.');
+// if (gameOfThrones === 'no' || gameOfThrones === 'n') {
+//     //if its correct give them a point
+//     userPoints++; //If the answer is yes, they're going to get a point
+// } else {
+//     //if its not correct, tell them to try again
+//     alert('Sorry, but I have never seen an episode of that show.. Try again! :(');
 
-}
+// }
+// alert(' You have ' + userPoints + ' points.')
+
+// //Question 5
+
+// var otherLanguage = prompt(' Does Ana speak any languages other than English?' , 'type yes or no').toLowerCase();
+
+// console.log(otherLanguage);
+
+// if (otherLanguage === 'yes' || otherLanguage === 'y') {
+//     //if its correct give them a point
+//     userPoints++; //If the answer is yes, they're going to get a point
+// } else {
+//     //if its not correct, tell them to try again
+//     alert('Wrong, Ana knows how to speak Spanish.');
+
+// }
 
 
 
 
-if (userPoints >= 3) {
-   alert('You Passed!!');
-} else {
-   alert('You failed..Try again..:(');
-}
+// if (userPoints >= 3) {
+//    alert('You Passed!!');
+// } else {
+//    alert('You failed..Try again..:(');
+// }
 
 
-alert(' You have ' + userPoints + ' points.')
+// alert(' You have ' + userPoints + ' points.')
